dfab2e3ae5dc2628448ad23b90ed989ff4abc199
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2023-09-15 16:51:02 -0400

    create img.data

--- src/Hero/Creations.jsx
+++ src/Hero/Creations.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import "./Creations.css"
+import I from "./Imgs";
 
 function Creations() {
     return (
-        <div>
-            <h1>Hello, World!</h1>
-        </div>
+        <main className="Creations--container">
+            <div className="Creations--box">
+                <div className="Creations--title">
+                    <h1>Our Creations</h1>
+                </div>
+                <div className="Creations--seeAll">
+                    <input type="button" value="See All" />
+                </div>
+            </div>
+            <div className="Creations--images">
+                {I.map((item, index) => (
+                <div className="Creations--imgs" key={index} style={{ backgroundImage: `URL(${item.img})`}}>
+                    <h2>{item.title}</h2>
+                </div>
+                ))}
+            </div>
+        </main>
     )
 }
 
---
+++ src/Hero/Imgs.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const I = [{
+    title:"The curiosity",
+    img:"../src/assets/images/desktop/image-curiosity.jpg"
+},
+{
+    title:" The grid",
+    img:"../src/assets/images/desktop/image-grid.jpg"
+}]
+
+
+export default I;
