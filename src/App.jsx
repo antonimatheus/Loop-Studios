17ef73aca835be4469650809cf8ee7aa49e6a1a8
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2023-09-17 19:36:28 -0400

    started to create Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import Header from './Header/Header'
 import MainContent from './Hero/MainContent'
+import Footer from './Footer/Footer'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Header />
       <MainContent />
+      <Footer />
     </div>
   )
 }
