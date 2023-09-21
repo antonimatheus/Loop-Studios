623ac8703770e8728b0f86222f3131966840962b
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2023-09-21 17:36:58 -0400

    add mobile introduce/footer

--- src/Hero/Introduce.jsx
+++ src/Hero/Introduce.jsx
@@ -15,11 +15,21 @@ function Introduce() {
                 </div>
             </div>
 
-            <div className="Introduce--box">
+            <div className="Introduce--box1">
                 <div className="Introduce--title">
                     <h1>The leader in interactive VR</h1>
                 </div>
-
+                <div className="Introduce--text">
+                    <p>Founded in 2011, Loopstudios has been producing world-class virtual reality
+                    projects for some of the best companies around the globe. Our award-winning
+                    creations have transformed businesses through digital experiences that bind
+                    to their brand.</p>
+                </div>
+            </div>
+            <div className="Introduce--box2">
+                <div className="Introduce--title">
+                    <h1>The leader in interactive VR</h1>
+                </div>
                 <div className="Introduce--text">
                     <p>Founded in 2011, Loopstudios has been producing world-class virtual reality
                     projects for some of the best companies around the globe. Our award-winning
